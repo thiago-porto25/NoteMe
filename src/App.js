540d1c4d0fe99a519dcb146704d328f5ee3d3d62
thiago-porto25 <thiago_porto25@hotmail.com>
2021-09-18 11:04:18 -0300

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import UserContext from "./context/userContext"
+import { Switch, Route } from "react-router-dom"
+import { useAuthListener } from "./hooks"
 
 function App() {
+  const { user } = useAuthListener()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <UserContext.Provider value={user}>
+      <Switch>
+        <Route to="/"></Route>
+      </Switch>
+    </UserContext.Provider>
+  )
 }
 
-export default App;
+export default App
