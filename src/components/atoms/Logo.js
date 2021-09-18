4617336c62147d8a83a4e1b2a6bcd161aadbb67a
thiago-porto25 <thiago_porto25@hotmail.com>
2021-09-18 17:08:18 -0300

    finish logo atom

--- src/components/atoms/Logo.js
+++ src/components/atoms/Logo.js
@@ -1,5 +1,26 @@
-import React from "react"
+import styled from "styled-components"
+import { AiOutlineCheckSquare } from "react-icons/ai"
+
+const LogoText = styled.div`
+  display: flex;
+  align-items: center;
+  color: var(--primary);
+  font-size: 3rem;
+
+  h1 {
+    font-family: "Lora", sans-serif;
+    font-size: inherit;
+    margin: 0;
+  }
+`
 
 export default function Logo() {
-  return <div></div>
+  return (
+    <>
+      <LogoText>
+        <AiOutlineCheckSquare />
+        <h1>NoteMe</h1>
+      </LogoText>
+    </>
+  )
 }
