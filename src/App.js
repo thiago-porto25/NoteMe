ce2cac1da2ea45a9654adb972840d528f69f0656
thiago-porto25 <thiago_porto25@hotmail.com>
2021-09-18 16:13:23 -0300

    set up logic for protected routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import UserContext from "./context/userContext"
 import { Switch, Route } from "react-router-dom"
 import { useAuthListener } from "./hooks"
+import { ProtectedRoute, IsUserRedirect } from "./helpers"
+import * as ROUTES from "./constants/routes"
+import {
+  Home,
+  Login,
+  Signup,
+  Settings,
+  ForgotPassword,
+  NotFound
+} from "./components/pages"
 
 function App() {
   const { user } = useAuthListener()
@@ -8,7 +18,41 @@ function App() {
   return (
     <UserContext.Provider value={user}>
       <Switch>
-        <Route to="/"></Route>
+        <ProtectedRoute user={user} exact path={ROUTES.HOME}>
+          <Home />
+        </ProtectedRoute>
+
+        <ProtectedRoute user={user} path={ROUTES.SETTINGS}>
+          <Settings />
+        </ProtectedRoute>
+
+        <IsUserRedirect
+          path={ROUTES.LOGIN}
+          user={user}
+          loggedInPath={ROUTES.HOME}
+        >
+          <Login />
+        </IsUserRedirect>
+
+        <IsUserRedirect
+          path={ROUTES.SIGNUP}
+          user={user}
+          loggedInPath={ROUTES.HOME}
+        >
+          <Signup />
+        </IsUserRedirect>
+
+        <IsUserRedirect
+          path={ROUTES.FORGOT_PASSWORD}
+          user={user}
+          loggedInPath={ROUTES.HOME}
+        >
+          <ForgotPassword />
+        </IsUserRedirect>
+
+        <Route>
+          <NotFound />
+        </Route>
       </Switch>
     </UserContext.Provider>
   )
---
+++ src/constants/routes.js
@@ -0,0 +1,11 @@
+export const HOME = "/"
+
+export const LOGIN = "/login"
+
+export const SIGNUP = "/signup"
+
+export const SETTINGS = "/settings"
+
+export const FORGOT_PASSWORD = "/forgot-password"
+
+export const NOT_FOUND = "/not-found"
