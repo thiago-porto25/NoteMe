29222eeba9420c34e394e0daa00dbee874b738e4
thiago-porto25 <thiago_porto25@hotmail.com>
2021-09-22 12:00:29 -0300

    refactor using firebase newest logic

--- src/hooks/useAuthListener.js
+++ src/hooks/useAuthListener.js
@@ -1,23 +1,20 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect, useContext } from "react"
 import FirebaseContext from "../context/firebaseContext"
 
 export default function useAuthListener() {
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem("authUser")))
+  const [user, setUser] = useState(null)
   const { onAuthStateChanged, auth } = useContext(FirebaseContext)
 
   useEffect(() => {
-    const listener = onAuthStateChanged(auth, (authUser) => {
+    onAuthStateChanged(auth, (authUser) => {
       if (authUser) {
-        localStorage.setItem("authUser", JSON.stringify(authUser))
         setUser(authUser)
       } else {
-        localStorage.removeItem("authUser")
         setUser(null)
       }
     })
-
-    return () => listener()
-  }, [onAuthStateChanged, auth])
+  }, [])
 
   return { user }
 }
