178b624f4e9d77220f5bc5069f2d4390d2ef292a
thiago-porto25 <thiago_porto25@hotmail.com>
2021-09-22 19:27:12 -0300

    query for notes in creation order

--- src/hooks/useNotes.js
+++ src/hooks/useNotes.js
@@ -4,7 +4,7 @@ import FirebaseContext from "../context/firebaseContext"
 
 export default function useNotes({ uid = "" }) {
   const [userNotes, setUserNotes] = useState([])
-  const { db, onSnapshot, collection, query, where } =
+  const { db, onSnapshot, collection, query, where, orderBy } =
     useContext(FirebaseContext)
 
   useEffect(() => {
@@ -13,7 +13,11 @@ export default function useNotes({ uid = "" }) {
     const listenForNotes = async () => {
       const notesRef = await collection(db, "notes")
 
-      const q = await query(notesRef, where("authorId", "==", uid))
+      const q = await query(
+        notesRef,
+        where("authorId", "==", uid),
+        orderBy("createdAt", "asc")
+      )
 
       const unsub = await onSnapshot(q, (querySnapshot) => {
         setUserNotes([])
