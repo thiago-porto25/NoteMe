6372f254b567339a08c37c066054f48dae9ca26d
thiago-porto25 <thiago_porto25@hotmail.com>
2021-09-23 10:44:44 -0300

    install and use loading skeleton

--- src/components/organisms/NotesList.js
+++ src/components/organisms/NotesList.js
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import styled from "styled-components"
 import { NotesListItem } from "../molecules"
 import NotesContext from "../../context/notesContext"
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
 
 const NotesListContainer = styled.div`
   overflow-y: scroll;
@@ -22,14 +23,20 @@ export default function NotesList() {
 
   return (
     <NotesListContainer>
-      {userNotes.map((note, i) => (
-        <NotesListItem
-          currentNote={currentNote}
-          setCurrentNote={setCurrentNote}
-          note={note}
-          key={`${note}-${i}`}
-        />
-      ))}
+      {userNotes.length ? (
+        userNotes.map((note, i) => (
+          <NotesListItem
+            currentNote={currentNote}
+            setCurrentNote={setCurrentNote}
+            note={note}
+            key={`${note}-${i}`}
+          />
+        ))
+      ) : (
+        <SkeletonTheme color="#444" highlightColor="#555">
+          <Skeleton count={3} height={70} />
+        </SkeletonTheme>
+      )}
     </NotesListContainer>
   )
 }
